bef6ef61f50baf5da073eb491fde6d65984713e0
Humakt83 <urlact@yahoo.com>
2019-04-21 12:33:37 +0300

    build

---
+++ build/precache-manifest.3671a3cd6ac22ea0d6aaada027f637b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde4ccb7ec3833e3e548",
+    "url": "/static/js/main.d7566511.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "92606100be59aaac889f",
+    "url": "/static/css/2.4b8cef20.chunk.css"
+  },
+  {
+    "revision": "92606100be59aaac889f",
+    "url": "/static/js/2.44012f17.chunk.js"
+  },
+  {
+    "revision": "8914b8e9028ce6c7acef49fdde2b414f",
+    "url": "/index.html"
+  }
+];
